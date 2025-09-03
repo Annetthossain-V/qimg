5f9993f8ca7263ac9d3e43e6fb4d235a9e1383bc
Annett Hossain <annetthossain@outlook.com>
2025-09-03 14:31:14 +0600

    finally got it working

---
+++ env.sh
@@ -0,0 +1,4 @@
+setenv OPENCV_LIB_DIR /home/annetthossain/software/opencv/lib 
+setenv OPENCV_INCLUDE_DIR /home/annetthossain/software/opencv/include/opencv4/ 
+setenv OpenCV_DIR /home/annetthossain/software/ 
+cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{
+    core,
+    imgcodecs,
+    imgproc,
+    prelude::*,
+    Result,
+};
+use std::io::Result;
+
+fn main() -> Result<()> { 
+
+    let img = imgcodecs::imread("photo.jpg", imgcodecs::IM_COLOR);
+
+    Ok(())
 }
